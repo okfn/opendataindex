585fd03b81e9032e9e8ae539acda343e6d801ade
Paul Walsh <paulywalsh@gmail.com>
2014-10-17 11:19:41 +0300

    Small docs changes on custom jinja filters

--- utilities/filters.py
+++ utilities/filters.py
@@ -1,16 +1,21 @@
-"""Jinja filters."""
+"""Custom Jinja filters."""
+
+
 import jinja2
 import markdown as mdlib
 
 
 def markdown(content):
+    """Parse `content` as markdown."""
+
     return jinja2.Markup(mdlib.markdown(content))
 
 
 def where(iterable, key, value):
-    """This is a POC, it simply expects an iterable of dicts.
+    """This is a POC that expects an iterable of dicts only.
 
     Filter `iterable` on `key` where `key` is `value`
 
     """
+
     return [o for o in iterable if o[key] == value]
