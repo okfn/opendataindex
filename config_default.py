b863e5ad3a0d6d2ea7a54fefa394805d30e97cac
Paul Walsh <paulywalsh@gmail.com>
2014-11-04 21:51:37 +0200

    Add hooks for non-committed configuration per instance.

--- config_default.py
+++ config_default.py
@@ -5,53 +5,30 @@ import os
 import sys
 import datetime
 
-
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 UTILITIES = os.path.join(PROJECT_ROOT, 'utilities')
 sys.path.insert(1, UTILITIES)
-
 import filters
 
 LOAD_CONTENT_CACHE = False
 AUTORELOAD_IGNORE_CACHE = True
-
 AUTHOR = u'Open Knowledge'
 SITENAME = u'Open Data Index'
 SITEURL = u''
-DEFAULT_LANG = 'en'
-LANGUAGES = ['en', 'es']
-
+DEFAULT_LANG = u'en'
 STATIC_PATHS = ['extra/CNAME']
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 PATH = 'content'
 OUTPUT_PATH = 'output'
-
-TIMEZONE = 'Europe/Paris'
-
-DEFAULT_LANG = u'en'
-
-# Feed generation is usually not desired when developing
+TIMEZONE = 'UTC'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
+LINKS = (('', ''),)
+SOCIAL = (('', ''),)
 DEFAULT_PAGINATION = False
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
 PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
-
 AUTHOR_SAVE_AS = False
 AUTHORS_SAVE_AS = False
 CATEGORY_SAVE_AS = False
@@ -59,23 +36,35 @@ CATEGORIES_SAVE_AS = False
 TAG_SAVE_AS = False
 TAGS_SAVE_AS = False
 ARCHIVES_SAVE_AS = False
-
-PLUGIN_PATHS = [os.path.join(PROJECT_ROOT, 'plugins')]
-PLUGINS = ['datastore', 'datastore_api', 'datastore_assets', 'i18n_subsites',
-           'pelican_alias']
-
 THEME = os.path.join(PROJECT_ROOT, 'themes', 'odi')
 THEME_STATIC_DIR = 'static'
 THEME_STATIC_PATH = os.path.join(THEME, THEME_STATIC_DIR)
-
 DISPLAY_DATE_FORMAT = '%Y-%m-%d'
 DISPLAY_TIME_FORMAT = '%H:%M:%S'
 DISPLAY_DATETIME_FORMAT = '{0}T{1}'.format(DISPLAY_DATE_FORMAT,
                                            DISPLAY_TIME_FORMAT)
-
 TIMESTAMP = datetime.datetime.now().strftime(DISPLAY_DATE_FORMAT)
+JINJA_EXTENSIONS = [
+  'jinja2.ext.i18n',
+  'jinja2.ext.do',
+  'jinja2.ext.with_',
+  'jinja2.ext.loopcontrols'
+]
+JINJA_FILTERS = {
+  'where': filters.where,
+  'markdown': filters.markdown,
+  'natsort': filters.natsort
+}
+PLUGIN_PATHS = [os.path.join(PROJECT_ROOT, 'plugins')]
+PLUGINS = [
+  'datastore',
+  'datastore_api',
+  'datastore_assets',
+  'i18n_subsites',
+  'pelican_alias'
+]
 
-# Our DATASTORE PLUGIN settings
+# DATASTORE PLUGIN CONFIGURATION
 DATASTORE = {
     'location': os.path.join(PROJECT_ROOT, 'data'),
     'formats': ['.csv'],
@@ -100,12 +89,11 @@ DATASTORE = {
     }
 }
 
-# OPEN KNOWLEDGE SETTINGS
 OK = {
-    'ribbon': False
+    'ribbon': True
 }
 
-# OPEN DATA INDEX SETTINGS
+# OPEN DATA INDEX CONFIGURATION
 ODI = {
     'survey': {
       'name': u'Open Data Index Survey',
@@ -130,7 +118,7 @@ ODI = {
     'googleanalytics': u'',
     'mailinglist': u'',
     'cli':  {
-        'languages': LANGUAGES,
+        'languages': ['en', 'es'],
         'test_path': 'tests',
         'content_path': PATH,
         'output_path': OUTPUT_PATH,
@@ -150,15 +138,9 @@ ODI = {
     }
 }
 
-JINJA_EXTENSIONS = [
-  'jinja2.ext.i18n',
-  'jinja2.ext.do',
-  'jinja2.ext.with_',
-  'jinja2.ext.loopcontrols'
-]
-
-JINJA_FILTERS = {
-  'where': filters.where,
-  'markdown': filters.markdown,
-  'natsort': filters.natsort
-}
+# If `config_instance` exists, load it for instance-specific configuration.
+# See `config_instance.example` to get started.
+try:
+  from config_instance import *
+except ImportError:
+  pass
---
+++ config_instance.example.py
@@ -0,0 +1,49 @@
+# ODI = {
+#     'survey': {
+#       'name': u'Open Data Index Survey',
+#       'domain': u'http://global.census.okfn.org/',
+#       'submit_route': u'submit/'
+#     },
+#     'sponsor': {
+#       'name': u'Open Knowledge',
+#       'domain': u'https://okfn.org/',
+#     },
+#     'years': [u'2014', u'2013'],
+#     'current_year': u'2014',
+#     'na': u'n/a',
+#     'email': u'index@okfn.org',
+#     'description': u'The Open Data Index assesses the state of open government data around the world.',
+#     'twitter': '',
+#     'repo': u'https://github.com/okfn/opendataindex',
+#     'author': {
+#         'name': AUTHOR,
+#         'url': u'https://okfn.org/'
+#     },
+#     'googleanalytics': u'',
+#     'mailinglist': u'',
+#     'cli':  {
+#         'languages': LANGUAGES,
+#         'test_path': 'tests',
+#         'content_path': PATH,
+#         'output_path': OUTPUT_PATH,
+#         'trans_path': 'themes/odi/translations',
+#         'deploy_remote': 'upstream',
+#         'database': {
+#             'submissions': '',
+#             'entries': '',
+#             'questions': '',
+#             'datasets': '',
+#             'places': ''
+#         },
+#         'limited': {
+#             'places': ['au', 'gb'],
+#             'datasets': ['timetables', 'spending']
+#         }
+#     }
+# }
+
+# I18N_SUBSITES = {
+#     'es': {
+#         'SITENAME': ''
+#     }
+# }
