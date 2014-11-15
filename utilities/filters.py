2e5e6ba3d0f054d9fd23ee9c97f1170b4b868bd9
Paul Walsh <paulywalsh@gmail.com>
2014-11-15 21:47:55 +0200

    Table info and sorting.

--- utilities/filters.py
+++ utilities/filters.py
@@ -22,29 +22,21 @@ operators = {
 
 def markdown(content):
     """Parse `content` as markdown."""
-
     return jinja2.Markup(mdlib.markdown(content))
 
 
 def where(iterable, key, value, op='=='):
-    """This is a POC that expects an iterable of dicts only.
-
-    Filter `iterable` on `key` where `key` `op` `value`
-
-    """
-
+    """Filter `iterable` of dicts on `key` where `key` `op` `value`"""
     operator_func = operators[op]
     return [o for o in iterable if operator_func(o[key], value)]
 
 
 def natsort(iterable, attribute=None, reverse=False):
     """Like sort, but, all natural. For us, sorts strings as numbers."""
-
-    return natsortlib.natsorted(iterable, key=operator.itemgetter(attribute),
-                                reverse=reverse)
+    _key = operator.itemgetter(attribute)
+    return natsortlib.natsorted(iterable, key=_key, reverse=reverse)
 
 
 def tojson(content):
-    """."""
-
+    """Parse content as JSON. Does not handle errors."""
     return json.dumps(content)
